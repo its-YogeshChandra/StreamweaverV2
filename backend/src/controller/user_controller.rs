819b12e702eaacadb4a8cbd11f25188cf2ad8c63
its-YogeshChandra <pandittheroyal@gmail.com>
2026-03-23 01:14:58 +0530

    feat: Implement video upload endpoint using actix-multipart and a new user controller.

--- backend/src/controller/user_controller.rs
+++ backend/src/controller/user_controller.rs
@@ -1,36 +1,27 @@
 use actix_web::{
-    web, get, post, HttpResponse, Responder
+    post, HttpResponse, Responder
 };
-use actix_multipart::form::{json::Json as MpJson, tempfile::TempFile, MultipartForm};
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-struct Metadata {
-    name: String,
-}
-
+use actix_multipart::form::{MultipartForm, tempfile::TempFile, text::Text};
 
 #[derive(Debug, MultipartForm)]
-struct UploadVideo {
-    #[multipart(content_type = "video")]
+pub struct UploadVideo {
     #[multipart(limit = "100mb")]
     video: TempFile,
-   json_data: MpJson<Metadata> 
 }
 
 
 #[post("/upload")]
-async fn upload_video (MultipartForm(form): MultipartForm<UploadVideo>) -> impl Responder {
-    //check for the video file 
-    let file = form.video;
-    let file_name = form.json_data.name;
+pub async fn upload_video (MultipartForm(form): MultipartForm<UploadVideo>) -> impl Responder {
 
-    //write this video in to the public folder
-    let file_path = format!("public/{}", file_name);
-    file.write_to(&file_path).await.unwrap();
-    
-    
 
-    //create the controller for video upload 
-    HttpResponse::Ok().body("Hello World")
+   let file_name = form.video.file_name.unwrap().to_string();
+      println!("File name: {}", file_name);
+   
+  let destination_folder = format!("public/{}", file_name);
+
+   //write the video into the destination folder 
+   form.video.file.persist(&destination_folder).unwrap(); 
+
+HttpResponse::Ok().body("Video uploaded successfully")            
+        //create the controller for video upload 
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,8 @@
 use actix_web::{App, HttpServer, middleware::Logger};
 use std::io::Result;
+mod controller;
 
+use crate::controller::upload_video;
 
 //create the actix server 
 #[actix_web::main]
@@ -9,6 +11,7 @@ async fn main() -> Result<()>{
 HttpServer::new(||{
         App::new()
         .wrap(Logger::default())
+        .service(upload_video)
  //all the routes of the controller
 })
 .bind(("0.0.0.0", 8080))?
