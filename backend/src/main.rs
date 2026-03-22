478078952b516eee474adcd40ceb6e011e83d79c
its-YogeshChandra <pandittheroyal@gmail.com>
2026-03-22 22:42:52 +0530

    feat: Set up an initial Actix-web server and add `README.md` to `.gitignore`.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, middleware::Logger, web};
+use std::io::Result;
+
+
+//create the actix server 
+#[actix_web::main]
+async fn main() -> Result<()>{
+
+HttpServer::new(||{
+        App::new()
+        .wrap(Logger::default())
+ //all the routes of the controller
+})
+.bind(("0.0.0.0", 8080))?
+.run()
+.await 
 }
+
