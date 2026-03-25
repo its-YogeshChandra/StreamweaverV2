54ffc3ac9f03f6f5a681c224f5c7ba43543fbf09
its-YogeshChandra <pandittheroyal@gmail.com>
2026-03-26 02:14:18 +0530

    refactor: `upload_to_cloud` now internally defines and iterates output paths for HLS, sprites, and chapters.

--- worker/src/utils/upload_utility.rs
+++ worker/src/utils/upload_utility.rs
@@ -2,16 +2,24 @@
 
 
 //cloudflare r2 for the bucket  
-pub fn upload_to_cloud(job_id: &str, hls_output: &str, chapters: &Vec<Chapter>, sprites: &Vec<String>) -> Result<(), String> {
+pub fn upload_to_cloud(job_id: &str) -> Result<(), String> {
     
-    //the thing to fix the issue
+    //get the hls output
+    let hls_output_path = "media/output/video";
+    let sprite_output_path = "media/output/sprites";
+    let chapter_output_path = "media/output/chapters";
+   
+    //put the path into a vector 
+    let paths = vec![hls_output_path, sprite_output_path, chapter_output_path];
 
-    //connect to the cloudflare r2 
+    //loop through the vector and upload each file to the cloud
+    for path in paths {
+       //read the file from each path 
+            //read the file whose name match the job id 
+        
 
-    //input path 
-
-    //upload the hls ouptput, sprites and chapters to the cloud
-
+       //upload it into the cloud 
+    }
 
 }
 
