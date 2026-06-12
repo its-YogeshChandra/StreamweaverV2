187ce22d7b7c755535d8bc7140c9addd071b1225
its-YogeshChandra <pandittheroyal@gmail.com>
2026-06-12 23:35:41 +0530

    feat: implement media downloading with destination path support and error handling

--- worker/src/utils/media_bucket_utility.rs
+++ worker/src/utils/media_bucket_utility.rs
@@ -1,13 +1,22 @@
-//get the data from the redis client 
+use reqwest::Client;
 
-//download the video from media bucket 
 
-//load the data into temp folder 
-
-//return the essential data back 
-
-pub async fn get_from_media_bucket(media_url : &str, job_id : &str) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn get_from_media_bucket(media_url : &str, job_id : &str, destination_folder_path: &str) -> Result<(), Box<dyn std::error::Error>> {
  //get the data from the media bucket 
- let media_file = reqwest::get(media_url).await?.bytes().await?;
-    Ok(())
+  //create the reqwest client 
+  let client = Client::new();
+
+  //get the media 
+  let response = client.get(media_url).send().await?;
+
+  //check for the response status 
+  if !response.status().is_success() { 
+    return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Failed to download media")));
+  }
+
+  //read the response and write into destination folder 
+  let media = response.bytes().await?;
+  tokio::fs::write(destination_folder_path, &media).await?;
+  
+     Ok(())
 }
